3763d3b5acb610319e25d3f968773b77d86e7ce8
Xiang Li <quic_lixian@quicinc.com>
2025-08-19 15:10:53 +0800

    hab: Skip sending unimport message during GVM restart
    During GVM restart, pchan->otherend_closed is set, making it expected
    that unimport sync messages cannot be sent. However, HAB currently
    logs these cases as errors.
    
    This patch skips sending unimport sync messages when otherend_closed
    is set for a physical channel, reducing noise in the logs during
    restart scenarios.
    
    Change-Id: I99c6705053a65a8f6f184a3966ab362286e04615
    Signed-off-by: Xiang Li <quic_lixian@quicinc.com>

--- os/linux/hab_mem_linux.c
+++ os/linux/hab_mem_linux.c
@@ -205,12 +205,12 @@ static void pages_list_destroy(struct kref *refcount)
 		HAB_HEADER_SET_ID(header, HAB_VCID_UNIMPORT);
 		HAB_HEADER_SET_SESSION_ID(header, HAB_SESSIONID_UNIMPORT);
 		ret = physical_channel_send(pglist->pchan, &header, &pglist->export_id);
-		if (ret)
+		if (ret && (ret != -ENODEV))
 			pr_err("unimp msg sent fail %d, vcid %x, expid %d, pg nr %ld\n",
 				ret, pglist->vcid, pglist->export_id, pglist->npages);
 		else
-			pr_debug("unimp msg sent, expid %d, vcid %x, pg nr %ld\n",
-			    pglist->export_id, pglist->vcid, pglist->npages);
+			pr_debug("pchan send ret %d, expid %d, vcid %x, pg nr %ld\n",
+			    ret, pglist->export_id, pglist->vcid, pglist->npages);
 	}
 
 	vfree(pglist->pages);
